08c942b9a049b16f1fafbb92e76b433f9daf5bde
dty4work <121413977+dty4work@users.noreply.github.com>
2023-01-27 17:20:35 +0000

    add heap demonstration

--- Ch03_Ownership/2_heap_and_stack/src/main.rs
+++ Ch03_Ownership/2_heap_and_stack/src/main.rs
@@ -1,16 +1,25 @@
-const MAX_VALUE: i32 = 40_000;
+// const MAX_VALUE: i32 = 40_000;
+
+// fn main() {
+//     let (x, y) = (2, 4);
+//     let sum_value = square_sum(x, y);
+//     println!("The value of the Square of sum = {}", sum_value);
+// }
+
+// fn square_sum(num1: i32, num2: i32) -> i32 {
+//     let result = square(num1 + num2);
+//     result
+// }
+
+// fn square(num: i32) -> i32 {
+//     num * num
+// }
 
 fn main() {
-    let (x, y) = (2, 4);
-    let sum_value = square_sum(x, y);
-    println!("The value of the Square of sum = {}", sum_value);
-}
+    let x: i32 = 5;
 
-fn square_sum(num1: i32, num2: i32) -> i32 {
-    let result = square(num1 + num2);
-    result
-}
-
-fn square(num: i32) -> i32 {
-    num * num
+    let s1 = String::from("some string");
+    let s2 = s1;
+    let s3 = &s2;
+    let s4 = s2.clone();
 }
