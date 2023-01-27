0e5af23182352a9c6a23a0424ff270861ff69287
dty4work <121413977+dty4work@users.noreply.github.com>
2023-01-27 17:19:04 +0000

    add stack calls demonstration

--- Ch03_Ownership/2_heap_and_stack/src/main.rs
+++ Ch03_Ownership/2_heap_and_stack/src/main.rs
@@ -1,3 +1,16 @@
+const MAX_VALUE: i32 = 40_000;
+
 fn main() {
-    println!("Hello, world!");
+    let (x, y) = (2, 4);
+    let sum_value = square_sum(x, y);
+    println!("The value of the Square of sum = {}", sum_value);
+}
+
+fn square_sum(num1: i32, num2: i32) -> i32 {
+    let result = square(num1 + num2);
+    result
+}
+
+fn square(num: i32) -> i32 {
+    num * num
 }
